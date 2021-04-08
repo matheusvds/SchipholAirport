20df36a2b9d0d06982b1542139b4a7fac6266d0f
Matheus Sousa <msousa@dunelm.com>
2021-04-08 20:11:55 +0100

    improve filtering readability code

--- Application/Scenes/ReachableAirports/ReachableAirportsPresenter.swift
+++ Application/Scenes/ReachableAirports/ReachableAirportsPresenter.swift
@@ -19,30 +19,38 @@ extension ReachableAirportsPresenter: ReachableAirportsPresentationLogic {
             let flights = response.flights,
             let schipholAirport = airports.first(where: { $0.id == "AMS" })
         else { return }
-        let schipholAirportLocation = CLLocation(latitude: schipholAirport.latitude, longitude: schipholAirport.longitude)
         
         let schipholDestinations = flights.filter { $0.departureAirportId == "AMS"  }.map { $0.arrivalAirportId }
         let destinationsSet = Set(schipholDestinations)
         
         let reachableAirports = airports.filter { destinationsSet.contains($0.id) }
         
-        let sortedAirports = reachableAirports.sorted { (first, second) -> Bool in
-            let firstLocation = CLLocation(latitude: first.latitude, longitude: first.longitude)
-            let secondLocation = CLLocation(latitude: second.latitude, longitude: second.longitude)
-            return schipholAirportLocation.distance(from: firstLocation) < schipholAirportLocation.distance(from: secondLocation)
-        }
+        let schipholAirportLocation = CLLocation(latitude: schipholAirport.latitude, longitude: schipholAirport.longitude)
+        
+        let sortedAirports: [ReachableAiport] = reachableAirports
+            .map {
+                let location = CLLocation(latitude: $0.latitude, longitude: $0.longitude)
+                let distance = location.distance(from: schipholAirportLocation)/1000
+                return ReachableAiport(id: $0.id, name: $0.name, location: location, distance: distance)
+            }
+            .sorted { $0.distance < $1.distance }
         
         let items: [DisplayedItems] = sortedAirports.map {
-            let location = CLLocation(latitude: $0.latitude, longitude: $0.longitude)
-            let distance = schipholAirportLocation.distance(from: location)/1000
-            return DisplayedItems(
+            DisplayedItems(
                 id: $0.id,
                 name: $0.name,
-                distance: "\(String(format:"%.02f", distance)) km"
+                distance: "\(String(format:"%.02f", $0.distance)) km"
             )
         }
-        
+
         let viewModel = ReachableAirports.GetAirports.ViewModel(items: items)
         viewController?.displayClosestsAirports(viewModel: viewModel)
     }
+}
+
+private struct ReachableAiport {
+    let id: String
+    let name: String
+    let location: CLLocation
+    let distance: Double
 }
